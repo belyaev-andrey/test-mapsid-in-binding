62ee2bdfb16cfc97cf22c084c4a5b71167e0bc1f
belyaev-andrey <belyaev.andrey@gmail.com>
2022-06-28 09:25:45 +0300

    Added code that fixes the issue

--- src/main/java/com/example/mapsidbinding/service/AllocationService.java
+++ src/main/java/com/example/mapsidbinding/service/AllocationService.java
@@ -3,6 +3,7 @@ package com.example.mapsidbinding.service;
 import com.example.mapsidbinding.entities.AllocationSector;
 import com.example.mapsidbinding.entities.AllocationSector_;
 import com.example.mapsidbinding.entities.Sector;
+import com.example.mapsidbinding.entities.Sector_;
 import com.example.mapsidbinding.repositories.AllocationSectorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
@@ -19,6 +20,14 @@ public class AllocationService {
     public List<AllocationSector> findBySectors(List<Sector> sectors) {
         return allocationSectorRepository.findAll(sectorFilter(sectors).and(fetchSector()));
     }
+
+//    public Specification<AllocationSector> sectorFilter(List<Sector> sectors) {
+//        return (root, query, criteriaBuilder) -> root.get(AllocationSector_.sector)
+//                .get(Sector_.id)
+//                .in(sectors.stream()
+//                        .map(Sector::getId)
+//                        .toList());
+//    }
 
     public Specification<AllocationSector> sectorFilter(List<Sector> sectors) {
         return (root, query, criteriaBuilder) -> root.get(AllocationSector_.sector).in(sectors);
