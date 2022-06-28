347804ecac212e2891703e921355f79955d8154f
belyaev-andrey <belyaev.andrey@gmail.com>
2022-06-28 08:55:05 +0300

    Better testing

--- src/test/java/com/example/mapsidbinding/TestMapsidInBindingApplicationTests.java
+++ src/test/java/com/example/mapsidbinding/TestMapsidInBindingApplicationTests.java
@@ -13,8 +13,11 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @TestPropertySource(locations = {"classpath:application-test.properties"})
@@ -28,8 +31,12 @@ class TestMapsidInBindingApplicationTests {
     @Sql(scripts = "classpath:data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void testService(@Autowired AllocationService service, @Autowired SectorRepository sectorRepository) {
         List<Sector> sectors = sectorRepository.findAll();
-        List<AllocationSector> allocationSectors = service.findBySectors(sectors);
+        List<AllocationSector> allocationSectors = service.findBySectors(sectors)
+                .stream().sorted((s1, s2) -> s1.getName().compareToIgnoreCase(s2.getName()))
+                .toList();
         assertEquals(2, allocationSectors.size());
+        assertEquals("alloc sector 1", allocationSectors.get(0).getName());
+        assertEquals("alloc sector 2", allocationSectors.get(1).getName());
     }
 
 
